45060b8b82b3fc4b1b5386e156845ec7daa4fcac
salog0d <david.salomon.nava11@gmail.com>
2025-07-11 23:35:26 -0600

    Se definio la clase base para la solucion de captchas, integrando la api key proximo feature: hacer esta clase robusta para solucionar captchas dentro del flujo de todo el modulo

---
+++ utilities/anticaptcha.py
@@ -0,0 +1,19 @@
+from anticaptchaofficial.recaptchav2proxyless import *
+
+solver = recaptchaV2Proxyless()
+solver.set_verbose(1)
+solver.set_key("c5e932f34fb92d6cc9d5eb6a441afbee")
+solver.set_website_url("https://website.com")
+solver.set_website_key("SITE_KEY")
+#set optional custom parameter which Google made for their search page Recaptcha v2
+#solver.set_data_s('"data-s" token from Google Search results "protection"')
+
+# Specify softId to earn 10% commission with your app.
+# Get your softId here: https://anti-captcha.com/clients/tools/devcenter
+solver.set_soft_id(0)
+
+g_response = solver.solve_and_return_solution()
+if g_response != 0:
+    print("g-response: "+g_response)
+else:
+    print("task finished with error "+solver.error_code)
