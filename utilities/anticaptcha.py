f5bb357da68526651c3472eb44e6c21f656938e1
salog0d <david.salomon.nava11@gmail.com>
2025-07-14 15:46:02 -0600

    Defined Cptcha solver class and method

--- utilities/anticaptcha.py
+++ utilities/anticaptcha.py
@@ -2,23 +2,24 @@ from anticaptchaofficial.recaptchav2proxyless import *
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+class RecaptchaSolver:
+    def __init__(self, website_url, website_key):
+        load_dotenv()
+        self.website_url = website_url
+        self.website_key = website_key
+    
+    def solve(self):
+        
+        solver = recaptchaV2Proxyless()
+        solver.set_verbose(1)
+        solver.set_key(os.getenv('ANTICAPTCHA_API_KEY'))
+        solver.set_website_url(self.website_url)
+        solver.set_website_key(self.website_key)
 
-
-solver = recaptchaV2Proxyless()
-solver.set_verbose(1)
-solver.set_key("c5e932f34fb92d6cc9d5eb6a441afbee")
-solver.set_website_url(os.getenv("CAPTCHA_API_KEY"))
-solver.set_website_key("SITE_KEY")
-#set optional custom parameter which Google made for their search page Recaptcha v2
-#solver.set_data_s('"data-s" token from Google Search results "protection"')
-
-# Specify softId to earn 10% commission with your app.
-# Get your softId here: https://anti-captcha.com/clients/tools/devcenter
-solver.set_soft_id(0)
-
-g_response = solver.solve_and_return_solution()
-if g_response != 0:
-    print("g-response: "+g_response)
-else:
-    print("task finished with error "+solver.error_code)
+        g_response = solver.solve_and_return_solution()
+        if g_response != 0:
+            print("g-response: " + g_response)
+            return g_response
+        else:
+            print("Error: " + solver.error_code)
+            return None
